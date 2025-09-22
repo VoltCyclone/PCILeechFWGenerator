e7cb2f33864ae1130c05687ac1453e6c6094a04c
Ramsey McGrath <2006510+ramseymcgrath@users.noreply.github.com>
2025-09-21 20:08:16 -0400

    bugfix and nicer logs (#392)
    * ..
    
    Signed-off-by: Ramsey McGrath <ramseymcgrath@gmail.com>
    
    * version
    
    Signed-off-by: Ramsey McGrath <ramseymcgrath@gmail.com>
    
    ---------
    
    Signed-off-by: Ramsey McGrath <ramseymcgrath@gmail.com>

--- src/__version__.py
+++ src/__version__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Version information for PCILeech Firmware Generator."""
 
-__version__ = "0.13.25"
-__version_info__ = (0, 13, 25)
+__version__ = "0.13.26"
+__version_info__ = (0, 13, 26)
 
 # Release information
 __title__ = "PCILeech Firmware Generator"
@@ -13,5 +13,5 @@ __license__ = "MIT"
 __url__ = "https://github.com/voltcyclone/PCILeechFWGenerator"
 
 # Build metadata
-__build_date__ = "2025-09-20T06:25:07.569595"
-__commit_hash__ = "3866411"
+__build_date__ = "2025-09-21T23:22:59.651246"
+__commit_hash__ = "7fd9609"
